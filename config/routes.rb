b52d02e3d472509e6478993484f9e48bc4c3e256
tam1006 <tamusyun1006@gmail.com>
2021-08-20 13:42:00 +0900

    first init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  root 'posts#index'
+end
